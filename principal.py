34e25fdeb9ecf705e5ee2879f040cdbd257189f5
GarciaFranc0 <46821683@comunidadort.edu.ar>
2026-08-23 01:43:55 -0300

    Corrección en funciones y creacion del metodo calcular_promedio

--- principal.py
+++ principal.py
@@ -29,12 +29,13 @@ def mostrar_materias(id):
     else:
         print("La materia no existe.")
 
-def mostrar_calificaciones():
-    if calificaciones:
-        for calificacion in calificaciones:
-            print(calificacion)
+def mostrar_calificaciones(id_estudiante):
+    calificaciones_estudiante = [cal for cal in calificaciones if cal[1] == id_estudiante]
+    if calificaciones_estudiante:
+        for calificacion in calificaciones_estudiante:
+            print(estudiantes[calificacion[1]-1][1], "obtuvo", calificacion[2], "en", materias[calificacion[0]-1][1])
     else:
-        print("No hay calificaciones registradas.")
+        print("No hay calificaciones registradas para este estudiante.")
 
 def buscar_estudiante(id):
     estudiante_encontrado = None
@@ -58,15 +59,13 @@ def buscar_materia(id):
 
 def buscar_calificacion(id_materia, id_estudiante):
     calificacion_encontrada = None
-    if calificaciones:
-        for calificacion in calificaciones:
-            if calificacion[0] == id_materia and calificacion[1] == id_estudiante:
-                calificacion_encontrada = calificacion
-        if calificacion_encontrada not in calificaciones:
-            calificacion_encontrada = "La calificación no existe."
-    else:
-        calificacion_encontrada = "No hay calificaciones registradas."
+    for calificacion in calificaciones:
+        if calificacion[0] == id_materia and calificacion[1] == id_estudiante:
+            calificacion_encontrada = calificacion
+    if calificacion_encontrada not in calificaciones:
+        calificacion_encontrada = "La calificación no existe."
     return calificacion_encontrada
+
 
 
 def eliminar_estudiante(id):
@@ -88,15 +87,13 @@ def eliminar_materia(id):
         print("La materia no existe.")
 
 def eliminar_calificacion(id_materia, id_estudiante):
-    if calificaciones:
-        for calificacion in calificaciones:
-            if calificacion[0] == id_materia and calificacion[1] == id_estudiante:
-                calificaciones.remove(calificacion)
-                print("Calificación eliminada correctamente.")
-        if [id_materia, id_estudiante] not in [[cal[0], cal[1]] for cal in calificaciones]:
-            print("La calificación no existe.")
-    else:
-        print("No hay calificaciones registradas.")
+    for calificacion in calificaciones:
+        if calificacion[0] == id_materia and calificacion[1] == id_estudiante:
+            calificaciones.remove(calificacion)
+            print("Calificación eliminada correctamente.")
+    if [id_materia, id_estudiante] not in [[cal[0], cal[1]] for cal in calificaciones]:
+        print("La calificación no existe.")
+
 
 def actualizar_estudiante(id, nuevo_nombre, nuevo_apellido, nuevo_legajo):
     for estudiante in estudiantes:
@@ -119,20 +116,27 @@ def actualizar_materia(id, nueva_materia):
 
 
 def actualizar_calificacion(id_materia, id_estudiante, nueva_calificacion):
-    if calificaciones:
         for calificacion in calificaciones:
             if calificacion[0] == id_materia and calificacion[1] == id_estudiante:
                 calificacion[2] = nueva_calificacion
                 print("Calificación actualizada correctamente.")
         if [id_materia, id_estudiante] not in [[cal[0], cal[1]] for cal in calificaciones]:
             print("La calificación no existe.")
-    else:   
-        print("No hay calificaciones registradas.")
+
+def calcular_promedio(id_estudiante):
+    calificaciones_estudiante = [cal[2] for cal in calificaciones if cal[1] == id_estudiante]
+    if calificaciones_estudiante:
+        print(f"El promedio de el/la estudiante {estudiantes[id_estudiante-1][1]} es: {sum(calificaciones_estudiante) / len(calificaciones_estudiante)}")
+    else:
+        print("El estudiante no tiene calificaciones registradas.")
+
 
 
 # Matrices de entidades
 estudiantes = [[1, "Franco", "Garcia", 1001], [2, "Maria", "Lopez", 1002], [3, "Juan", "Perez", 1003], [4, "Ana", "Gomez", 1004], [5, "Luis", "Martinez", 1005]]
 materias = [[1, "Programacion"], [2, "Matematicas"], [3, "Fisica"], [4, "Quimica"], [5, "Historia"]]
-calificaciones = []
+calificaciones = [[1, 1, 10], [2, 1, 5], [3, 1, 2], [4, 1, 3], [5, 1, 9], [1, 2, 2], [2, 2, 4], [3, 2, 5], [4, 2, 7], [5, 2, 3], [1, 3, 4], [2, 3, 6], [3, 3, 2], [4, 3, 10], [5, 3, 0], [1, 4, 8], [2, 4, 9], [3, 4, 8], [4, 4, 7], [5, 4, 5], [1, 5, 10], [2, 5, 9], [3, 5, 2], [4, 5, 3], [5, 5, 0]]
 
 # Menu principal
+mostrar_calificaciones(2)
+prom = calcular_promedio(2)
