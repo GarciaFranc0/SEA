7cd484510d451edb8c2dccfdb376bc0bca32e0ed
GarciaFranc0 <46821683@comunidadort.edu.ar>
2026-08-23 00:09:07 -0300

    Creacion de las matrices de las entidades

---
+++ principal.py
@@ -0,0 +1,7 @@
+# Funciones
+
+
+# Lista de entidades
+estudiantes = [[1, "Franco", "Garcia", 1001], [2, "Maria", "Lopez", 1002], [3, "Juan", "Perez", 1003], [4, "Ana", "Gomez", 1004], [5, "Luis", "Martinez", 1005]]
+materias = [[1, "Programacion"], [2, "Matematicas"], [3, "Fisica"], [4, "Quimica"], [5, "Historia"]]
+calificaciones = []
